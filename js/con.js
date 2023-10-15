9776de112359c81b397411378113c8d8cd2ad204
LeBrontoJames <keenanabigailidn@gmail.com>
2023-10-15 21:17:32 +0700

    added animation to all pages

---
+++ js/con.js
@@ -0,0 +1,22 @@
+const hello_arr = ['CONTRIBUTE'];
+const next = document.getElementById('next')
+
+
+window.onload = function () {
+    const spanHelloContainer = document.querySelector(".span-hello-container");
+    const spanHello = document.querySelector(".span-hello");
+
+    let currentIndex = 0;
+
+    function displayNextHello() {
+        if (currentIndex < hello_arr.length) {
+            spanHello.textContent = hello_arr[currentIndex];
+            currentIndex++;
+            setTimeout(displayNextHello, 1000);
+        } else {
+            spanHelloContainer.classList.add("translate-animation");
+        }
+    }
+
+    displayNextHello();
+};
